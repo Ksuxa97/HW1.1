c38c9d4b5ec474bc155f8f49b33fc33e80096fcf
Kseniya Ignatyeva <kignatyeva@ourgapps.com>
2022-03-07 14:43:30 +0300

    done

--- HW1.1/ViewController.swift
+++ HW1.1/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var controlButton: UIButton!
+    
     override func viewDidLoad() {
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        redLight.layer.cornerRadius = 64
+        yellowLight.layer.cornerRadius = 64
+        greenLight.layer.cornerRadius = 64
+        
+        controlButton.setTitle("Start", for: .normal)
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func controlButtonPressed() {
+        if redLight.alpha == 1 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        } else if yellowLight.alpha == 1 {
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        } else if greenLight.alpha == 1 {
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+        } else {
+            controlButton.setTitle("Next", for: .normal)
+            redLight.alpha = 1
+        }
+    }
 }
 
